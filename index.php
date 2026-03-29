bb94aa87a8d6cfffb73e293de631ec08d97b988d
Jared Ramirez <20251207@itla.edu.do>
2026-03-29 14:34:31 -0400

    Cambios en index.php y css

--- index.php
+++ index.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Jared Ramirez">
-    <title>Comentarios</title>
+    <title>Deja tu comentario</title>
 
-    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    
-    <!-- Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
     <link rel="stylesheet" href="assets/css/style.css">
@@ -17,7 +14,6 @@
 
 <body class="bg-light">
 
-<!-- NAVBAR -->
 <nav class="navbar navbar-dark bg-dark shadow-sm">
     <div class="container">
         <span class="navbar-brand fw-semibold">
@@ -32,7 +28,6 @@
 
 <div class="container mt-5" style="max-width: 700px;">
 
-    <!-- FORMULARIO -->
     <div class="card border-0 shadow-sm mb-4">
         <div class="card-body">
 
@@ -57,7 +52,6 @@
         </div>
     </div>
 
-    <!-- LISTA DE COMENTARIOS -->
     <div id="lista-comentarios"></div>
 
 </div>
